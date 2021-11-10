fe136568b13f2bab712ac0cf37688ba9d1210d91
abmartinezm <91500360+abmartinezm@users.noreply.github.com>
2021-11-10 12:58:53 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+const menu=document.querySelector('.menu');
+const title=document.querySelector('#title');
+const arrow=document.querySelector('#arrow');
+const IconBurger= document.querySelector('.nav');
+
+
+IconBurger.addEventListener('click',()=>{
+
+    console.log('pulsado')
+    
+        // arrow.classList.toggle('hide')
+
+
+        // title.classList.toggle('hide')
+
+        menu.classList.toggle('show')
+
+    
+        
+    
+    
+    
+    
+})
